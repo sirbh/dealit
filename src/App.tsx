9c5460bda06a8b06d8c9356b5e717dcca492eb86
saurabh chauhan <sourabh1202@gmail.com>
2024-03-10 20:08:24 +0200

    added basic project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, Grid, InputAdornment, TextField, TextFieldProps, Toolbar, Typography, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/SearchOutlined";
+import "./App.css";
+
+const SearchField = styled(TextField)<TextFieldProps>({
+  '& .MuiOutlinedInput-root': {
+    backgroundColor: "white",
+  },
+  width: "50%",
+});
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="static">
+        <Toolbar sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+        }}>
+          <Typography variant="h4" component="div" sx={{
+            position: "absolute",
+            left: "30px",
+          }}>
+            Deal-it
+          </Typography>
+          <SearchField
+            size="small"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              placeholder: "Search for products",
+            }}
+          />
+        </Toolbar>
+      </AppBar>
+      <Box>
+        <Grid container spacing={2}>
+          {
+            [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+              <Grid item xs={12} md={3} key={item}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://fastly.picsum.photos/id/494/200/200.jpg?hmac=U0wPc_GayOgCTqCyWzhy9Cc_mj8wZEPjLdqILTEl4AM"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                      across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
